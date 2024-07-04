ef060b83952030add64043ce712ebf1e0627753a
Dhruvilshah007 <sdhruvil72@gmail.com>
2024-07-04 23:31:33 +0530

    Changes TO resolve conflict

--- src/main/java/com/ds/expensetracker/cashbook/service/CashbookValidationService.java
+++ src/main/java/com/ds/expensetracker/cashbook/service/CashbookValidationService.java
@@ -4,21 +4,24 @@ import com.ds.expensetracker.authentication.model.User;
 import com.ds.expensetracker.authentication.repository.UserRepository;
 import com.ds.expensetracker.cashbook.model.Cashbook;
 import com.ds.expensetracker.cashbook.repository.CashbookRepository;
+import com.ds.expensetracker.common.constants.CommonConstants;
 import com.ds.expensetracker.exception.commonException.ApplicationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-
-@Service
 @RequiredArgsConstructor
+@Service
 public class CashbookValidationService {
+
     private final CashbookRepository cashbookRepository;
     private final UserRepository userRepository;
 
+
+    //Not used in cashbook Kept for reference purpose
     public Cashbook validateCashbookExists(Long cashbookPkId) {
-        return cashbookRepository.findById(cashbookPkId)
+        return (Cashbook) cashbookRepository.findByCashbookPkIdAndActiveFlag(cashbookPkId, CommonConstants.ACTIVE_FLAG)
                 .orElseThrow(() -> new ApplicationException(
                         HttpStatusCode.valueOf(404),
                         "Invalid Cashbook Id",
